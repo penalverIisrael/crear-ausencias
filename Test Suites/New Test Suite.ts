b72ce5004edbdfe9e19c723c610f0df9094e0fad
penalverIsrael <40454776+penalverIisrael@users.noreply.github.com>
2018-06-21 16:12:02 +0200

    report añadido

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-21T15:28:51</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c1f1e335-d5a4-401b-afe0-dd69056a1353</testSuiteGuid>
+   <testCaseLink>
+      <guid>824d6b0c-16d7-4fe8-b8c3-2c0dee3c3005</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9dfe559a-2e3a-4108-94ce-d6ae6964e6fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2CampoObligatorioSinRellenar</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0e6b1fc9-a11d-4aa2-8026-8a13ee909f24</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/5SolicitudCorrectaDiferenteMotivo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c5e2ccf3-c1a0-4256-aaff-c617a4385271</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/4SolicitudCorrecta</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>112be92d-0248-40f6-a67f-0eefc2be0e6b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3FechaIncorrecta</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
