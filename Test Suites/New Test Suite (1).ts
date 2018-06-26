2d61e02c7200c078656eaec02b6061af09ee8645
penalverIsrael <40454776+penalverIisrael@users.noreply.github.com>
2018-06-26 14:56:37 +0200

    b

---
+++ Test Suites/New Test Suite (1).ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite (1)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5bf3b57e-2694-48f0-9636-0cf9c7eb67f3</testSuiteGuid>
+   <testCaseLink>
+      <guid>196ca542-1b24-4fcb-a74e-12059277c2d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AUS-3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>63d50fd5-d714-42e5-a31f-5f9a655d23c5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AUS-2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e6f2c2b5-c988-4f9e-af09-e5462c974633</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AUS-1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
